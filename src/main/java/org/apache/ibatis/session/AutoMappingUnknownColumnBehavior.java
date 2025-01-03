e5eec3a4e8243e57564b98cfcc768c4e4d9f5198
vonlinee <vonlinee@163.com>
2025-01-03 21:47:45 +0800

    remove IbatisException.java

--- src/main/java/org/apache/ibatis/session/AutoMappingUnknownColumnBehavior.java
+++ src/main/java/org/apache/ibatis/session/AutoMappingUnknownColumnBehavior.java
@@ -80,10 +80,10 @@ public enum AutoMappingUnknownColumnBehavior {
    */
   private static String buildMessage(MappedStatement mappedStatement, String columnName, String property,
       Class<?> propertyType) {
-    return new StringBuilder("Unknown column is detected on '").append(mappedStatement.getId())
-        .append("' auto-mapping. Mapping parameters are ").append("[").append("columnName=").append(columnName)
-        .append(",").append("propertyName=").append(property).append(",").append("propertyType=")
-        .append(propertyType != null ? propertyType.getName() : null).append("]").toString();
+    return "Unknown column is detected on '" + mappedStatement.getId() +
+      "' auto-mapping. Mapping parameters are " + "[" + "columnName=" + columnName +
+      "," + "propertyName=" + property + "," + "propertyType=" +
+      (propertyType != null ? propertyType.getName() : null) + "]";
   }
 
   private static class LogHolder {
